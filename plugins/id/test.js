62876334f28b2b860a4bb60aa3f25fb445fd02f1
Diana Vilé <diana.vile@gmail.com>
2018-08-02 20:11:58 +0200

    add id module

---
+++ plugins/id/test.js
@@ -0,0 +1,40 @@
+const {Document} = require('../..')
+const plugin = require('.')
+
+describe('ID-plugin', () => {
+  it('should add an id to a headline', () => {
+    const input = unindent`
+      # Test <?: #my-id ?>
+    `
+    const doc = new Document(input)
+    doc.config.use(plugin)
+    const html = doc.render()
+    expect(html)
+      .to.have.selector('h1#my-id')
+  })
+
+  it('should add an id to a paragraph', () => {
+    const input = unindent`
+      p Test <?: #my-id ?>
+    `
+    const doc = new Document(input)
+    doc.config.use(plugin)
+    const html = doc.render()
+    expect(html)
+      .to.have.selector('p#my-id')
+  })
+
+  it('should add an id to a code block', () => {
+    const input = unindent`
+      ~~~ div #my-id
+      <p> Lorem ipsum dolor sit amet.</p>
+      ~~~
+    `
+    const doc = new Document(input)
+    doc.config.use(plugin)
+    const html = doc.render()
+    expect(html)
+      .to.have.selector('div#my-id')
+  })
+})
+
